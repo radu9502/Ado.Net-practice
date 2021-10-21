979aafa9c43d9204c898507918dadfc2016ca1ee
radu9 <radu9@DESKTOP-7OO9ITB>
2021-10-21 14:08:59 +0300

    Add project files.

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,206 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_previous = new System.Windows.Forms.Button();
+            this.btn_next = new System.Windows.Forms.Button();
+            this.textBox_id = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox_firstName = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox_lastName = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btn_add = new System.Windows.Forms.Button();
+            this.btn_delete = new System.Windows.Forms.Button();
+            this.btn_update = new System.Windows.Forms.Button();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.SuspendLayout();
+            // 
+            // btn_previous
+            // 
+            this.btn_previous.Location = new System.Drawing.Point(252, 26);
+            this.btn_previous.Name = "btn_previous";
+            this.btn_previous.Size = new System.Drawing.Size(63, 35);
+            this.btn_previous.TabIndex = 0;
+            this.btn_previous.Text = "<";
+            this.btn_previous.UseVisualStyleBackColor = true;
+            this.btn_previous.Click += new System.EventHandler(this.btn_previous_Click);
+            // 
+            // btn_next
+            // 
+            this.btn_next.Location = new System.Drawing.Point(321, 26);
+            this.btn_next.Name = "btn_next";
+            this.btn_next.Size = new System.Drawing.Size(63, 35);
+            this.btn_next.TabIndex = 1;
+            this.btn_next.Text = ">";
+            this.btn_next.UseVisualStyleBackColor = true;
+            this.btn_next.Click += new System.EventHandler(this.btn_next_Click);
+            // 
+            // textBox_id
+            // 
+            this.textBox_id.Enabled = false;
+            this.textBox_id.Location = new System.Drawing.Point(97, 107);
+            this.textBox_id.Name = "textBox_id";
+            this.textBox_id.Size = new System.Drawing.Size(44, 20);
+            this.textBox_id.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(54, 110);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(21, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "ID:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(36, 149);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "First name:";
+            // 
+            // textBox_firstName
+            // 
+            this.textBox_firstName.Location = new System.Drawing.Point(97, 149);
+            this.textBox_firstName.Name = "textBox_firstName";
+            this.textBox_firstName.Size = new System.Drawing.Size(189, 20);
+            this.textBox_firstName.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(36, 187);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(59, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Last name:";
+            // 
+            // textBox_lastName
+            // 
+            this.textBox_lastName.Location = new System.Drawing.Point(97, 184);
+            this.textBox_lastName.Name = "textBox_lastName";
+            this.textBox_lastName.Size = new System.Drawing.Size(189, 20);
+            this.textBox_lastName.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(39, 224);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(55, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Birth date:";
+            // 
+            // btn_add
+            // 
+            this.btn_add.Location = new System.Drawing.Point(12, 342);
+            this.btn_add.Name = "btn_add";
+            this.btn_add.Size = new System.Drawing.Size(105, 27);
+            this.btn_add.TabIndex = 10;
+            this.btn_add.Text = "Add";
+            this.btn_add.UseVisualStyleBackColor = true;
+            this.btn_add.Click += new System.EventHandler(this.btn_add_Click);
+            // 
+            // btn_delete
+            // 
+            this.btn_delete.Location = new System.Drawing.Point(148, 342);
+            this.btn_delete.Name = "btn_delete";
+            this.btn_delete.Size = new System.Drawing.Size(105, 27);
+            this.btn_delete.TabIndex = 11;
+            this.btn_delete.Text = "Delete";
+            this.btn_delete.UseVisualStyleBackColor = true;
+            this.btn_delete.Click += new System.EventHandler(this.btn_delete_Click);
+            // 
+            // btn_update
+            // 
+            this.btn_update.Location = new System.Drawing.Point(279, 342);
+            this.btn_update.Name = "btn_update";
+            this.btn_update.Size = new System.Drawing.Size(105, 27);
+            this.btn_update.TabIndex = 12;
+            this.btn_update.Text = "Update";
+            this.btn_update.UseVisualStyleBackColor = true;
+            this.btn_update.Click += new System.EventHandler(this.btn_update_Click);
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.CustomFormat = "";
+            this.dateTimePicker1.Location = new System.Drawing.Point(97, 218);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker1.TabIndex = 13;
+            this.dateTimePicker1.ValueChanged += new System.EventHandler(this.dateTimePicker1_ValueChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(396, 397);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.btn_update);
+            this.Controls.Add(this.btn_delete);
+            this.Controls.Add(this.btn_add);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox_lastName);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox_firstName);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox_id);
+            this.Controls.Add(this.btn_next);
+            this.Controls.Add(this.btn_previous);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_previous;
+        private System.Windows.Forms.Button btn_next;
+        private System.Windows.Forms.TextBox textBox_id;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox_firstName;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox_lastName;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btn_add;
+        private System.Windows.Forms.Button btn_delete;
+        private System.Windows.Forms.Button btn_update;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.Entity;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        Employee employee = new Employee();
+        int id = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void btn_next_Click(object sender, EventArgs e)
+        {
+            NextEmployee();
+
+        }
+        private void NextEmployee()
+        {
+            bool done = false;
+            //employee.EmployeeID = id;
+            while (!done)
+            {
+
+                using (NORTHWNDEntities db = new NORTHWNDEntities())
+                {
+                    if (db.Employees.Where(x => x.EmployeeID > id - 1).FirstOrDefault() != null)
+                    { 
+                        id++;
+                        employee = db.Employees.Where(x => x.EmployeeID == id).FirstOrDefault();
+                        if (employee != null)
+                        {
+                            textBox_id.Text = employee.EmployeeID.ToString();
+                            textBox_firstName.Text = employee.FirstName;
+                            textBox_lastName.Text = employee.LastName;
+                           // textBox_birthDate.Text = employee.BirthDate.ToString();
+                            dateTimePicker1.Value = employee.BirthDate.Value;
+                            done = true;
+                        }
+                       
+
+                    }
+                    else
+                    {
+                        MessageBox.Show("This is the last employee!", "Error");
+                        done = true;
+                        id--;
+                    }
+                }
+
+            }
+        }
+
+    
+
+        private void btn_add_Click(object sender, EventArgs e)
+        {
+            employee.FirstName = textBox_firstName.Text.Trim();
+            employee.LastName = textBox_lastName.Text.Trim();
+            employee.BirthDate = dateTimePicker1.Value;
+            //employee.BirthDate = DateTime.Parse(textBox_birthDate.Text);
+            using(NORTHWNDEntities db = new NORTHWNDEntities())
+            {
+                db.Employees.Add(employee);
+                db.SaveChanges();
+            }
+            id--;
+            NextEmployee();
+            MessageBox.Show("Employee added");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            id--;
+            NextEmployee();      
+
+        }
+
+        private void btn_previous_Click(object sender, EventArgs e)
+        {
+
+            if (id > 1)
+            {
+               bool done = false;
+               while(!done)
+               {
+                    
+                        id--;
+                        using (NORTHWNDEntities db = new NORTHWNDEntities())
+                        {
+                            employee = db.Employees.Where(x => x.EmployeeID == id).FirstOrDefault();
+                            if (employee != null)
+                            {
+                                textBox_id.Text = employee.EmployeeID.ToString();
+                                textBox_firstName.Text = employee.FirstName;
+                                textBox_lastName.Text = employee.LastName;
+                                dateTimePicker1.Value = employee.BirthDate.Value;
+                                //textBox_birthDate.Text = employee.BirthDate.ToString();
+                                done = true;
+                            }
+                            
+
+                        }
+               }
+            }
+            else MessageBox.Show("This is the first employee!", "Error");
+        }
+
+        private void btn_delete_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Are you sure you want to delete this employee?","Delete Empoyee", MessageBoxButtons.YesNo) == DialogResult.Yes) 
+            {
+                using(NORTHWNDEntities db = new NORTHWNDEntities())
+                {
+                    var entry = db.Entry(employee);
+                    if (entry.State == EntityState.Detached)
+                        db.Employees.Attach(employee);
+                    db.Employees.Remove(employee);
+                    db.SaveChanges();
+                    NextEmployee();
+
+                }
+            }
+        }
+
+        private void btn_update_Click(object sender, EventArgs e)
+        {
+            employee.FirstName = textBox_firstName.Text.Trim();
+            employee.LastName = textBox_lastName.Text.Trim();
+            employee.BirthDate = dateTimePicker1.Value;
+            using (NORTHWNDEntities db = new NORTHWNDEntities())
+            {
+                db.Entry(employee).State = EntityState.Modified;
+                db.SaveChanges();
+            }
+            MessageBox.Show("Employee updated");
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
